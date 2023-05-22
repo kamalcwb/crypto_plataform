708fc01697749672ec39b82eae82503b5abd4c30
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2023-05-22 12:03:18 -0300

    beta

--- crypto_platform_app/src/components/CardDeal.jsx
+++ crypto_platform_app/src/components/CardDeal.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-    return (
-        <div>
-            CardDeal
+const CardDeal = () => (
+    <section className={layout.section}>
+        <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>
+                Encontre o melhor investimento <br className="sm:block hidden" /> em poucos passos.
+            </h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                Nosso time de tecnologia antecipa tendências do mercado, para tornar a experiência de investir mais simples e acessível para todas as pessoas.
+            </p>
+
+            <Button styles={`mt-10`} />
         </div>
-    )
-}
 
-export default CardDeal
+        <div className={layout.sectionImg}>
+            <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+        </div>
+    </section>
+);
+
+export default CardDeal;
+
---
+++ crypto_platform_app/src/constants/index.js
@@ -0,0 +1,193 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, send, shield, star } from "../assets";
+
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "features",
+        title: "Serviços",
+    },
+    {
+        id: "product",
+        title: "Produtos",
+    },
+    {
+        id: "clients",
+        title: "Clientes",
+    },
+];
+
+export const features = [
+    {
+        id: "feature-1",
+        icon: star,
+        title: "Os melhores assessores do mercado",
+        content:
+            "Com a Assessoria Crypto, ajudamos as pessoas nas decisões relacionadas aos seus investimentos, sempre de acordo com seus objetivos e perfil.",
+    },
+    {
+        id: "feature-2",
+        icon: shield,
+        title: "Seu investimento seguro",
+        content:
+            "Nosso time de tecnologia antecipa tendências do mercado, para tornar a experiência de investir mais simples e acessível para todas as pessoas.",
+    },
+    {
+        id: "feature-3",
+        icon: send,
+        title: "Tudo em um lugar",
+        content:
+            "Serviços financeiros a favor dos seus investimentos. Tudo em um só lugar, para quem investe não depender mais dos bancos convencionais..",
+    },
+];
+
+export const feedback = [
+    {
+        id: "feedback-1",
+        content:
+            "Antes utilizavamos apenas formas convencionais para investimentos, com a Crypto, conseguimos aumentar nosso patrimonio em cerca de 23%.",
+        name: "Ana Paula Bittencourt",
+        title: "CEO Altis Sulamericana",
+        img: people01,
+    },
+    {
+        id: "feedback-2",
+        content:
+            "Com a ajuda dos assessores consegui uma visão clara do mercado crypto, e assim, planejando meu futuro.",
+        name: "João Guilheme Medeiros",
+        title: "Desenvolvedor Senior",
+        img: people02,
+    },
+    {
+        id: "feedback-3",
+        content:
+            "Com a crypto aumentei minhas reservas em cerca de 25%, com a segurança de sempre fazer o melhor investimento.",
+        name: "Carlos Cardoso Figueira",
+        title: "Empresário",
+        img: people03,
+    },
+];
+
+export const stats = [
+    {
+        id: "stats-1",
+        title: "Pessoas sendo gerenciadas por nós neste momento",
+        value: "5800+",
+    },
+    {
+        id: "stats-2",
+        title: "Contas abertas na última semana",
+        value: "80+",
+    },
+    {
+        id: "stats-3",
+        title: "Investimetos sob nossa gestão",
+        value: "R$230M+",
+    },
+];
+
+export const footerLinks = [
+    {
+        title: "Links",
+        links: [
+            {
+                name: "Conteúdo",
+                link: "https://www.crypto.com/content/",
+            },
+            {
+                name: "Como funciona",
+                link: "https://www.crypto.com/how-it-works/",
+            },
+            {
+                name: "Criar",
+                link: "https://www.crypto.com/create/",
+            },
+            {
+                name: "Explorar",
+                link: "https://www.crypto.com/explore/",
+            },
+            {
+                name: "Termos & Serviços",
+                link: "https://www.crypto.com/terms-and-services/",
+            },
+        ],
+    },
+    {
+        title: "Communidade",
+        links: [
+            {
+                name: "Central de Ajuda",
+                link: "https://www.crypto.com/help-center/",
+            },
+            {
+                name: "Parceiros",
+                link: "https://www.crypto.com/partners/",
+            },
+            {
+                name: "Sugestoes",
+                link: "https://www.crypto.com/suggestions/",
+            },
+            {
+                name: "Blog",
+                link: "https://www.crypto.com/blog/",
+            },
+            {
+                name: "Novidades",
+                link: "https://www.crypto.com/newsletters/",
+            },
+        ],
+    },
+    {
+        title: "Parceirias",
+        links: [
+            {
+                name: "Nossos parceiros",
+                link: "https://www.crypto.com/our-partner/",
+            },
+            {
+                name: "Seja nosso parceiro",
+                link: "https://www.crypto.com/become-a-partner/",
+            },
+        ],
+    },
+];
+
+export const socialMedia = [
+    {
+        id: "social-media-1",
+        icon: instagram,
+        link: "https://www.instagram.com/",
+    },
+    {
+        id: "social-media-2",
+        icon: facebook,
+        link: "https://www.facebook.com/",
+    },
+    {
+        id: "social-media-3",
+        icon: twitter,
+        link: "https://www.twitter.com/",
+    },
+    {
+        id: "social-media-4",
+        icon: linkedin,
+        link: "https://www.linkedin.com/",
+    },
+];
+
+export const clients = [
+    {
+        id: "client-1",
+        logo: airbnb,
+    },
+    {
+        id: "client-2",
+        logo: binance,
+    },
+    {
+        id: "client-3",
+        logo: coinbase,
+    },
+];
